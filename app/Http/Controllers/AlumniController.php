901bc05b8618464dac955f1c344c6bde5d70ea53
fatihjr9 <malikfatihibrahim@gmail.com>
2023-12-27 16:48:18 +0700

    last update

--- app/Http/Controllers/AlumniController.php
+++ app/Http/Controllers/AlumniController.php
@@ -11,7 +11,7 @@ class AlumniController extends Controller
     {
        $alumnis = Alumni::all();
     
-       return view('mahasiswa.index', compact('alumnis'));
+       return view('mahasiswa.alumni', compact('alumnis'));
     }
 
     public function create()
